28ff826aead68074b05903659a5a2240a6cb8e94
iamhashir <ihashirr@outlook.com>
2025-12-22 17:38:55 +0400

    [CPP6-EX00] finished

---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,15 @@
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+#include <string>
+
+class ScalarConverter{
+    private:
+        ScalarConverter();
+        ScalarConverter(const ScalarConverter &);
+        ScalarConverter &operator=(const ScalarConverter &);
+        ~ScalarConverter();
+    public:
+        static void convert(const std::string &);
+};
+
+#endif
