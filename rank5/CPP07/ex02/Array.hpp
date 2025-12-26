9f7fb395a433736c4548b8b277b831b7ffd398ba
ihashirr <ihashirr@gmail.com>
2025-12-26 04:19:31 +0400

    [cpp07-ex02 completed]

--- rank5/CPP07/ex02/Array.hpp
+++ rank5/CPP07/ex02/Array.hpp
@@ -1,19 +1,60 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
-#include <iostream>
-template <typename T>
-class Array{
-	public:
-		Array();
-		Array(const Array &);
-		Array &operator=(const Array &);
-		~Array();
-		
-		Array &operator[](const Array &);
-		size_t size()const;
-	private:
-		T* data;
-		size_t length;
-};
 
+#include <cstddef>
+#include <exception>
+
+template <typename T> class Array{
+	public:
+		Array():_arr(NULL),_length(0){}
+
+		Array(unsigned int length):_arr(NULL),_length(length){
+			if(_length > 0)
+				_arr = new T[_length]();
+		}
+		
+		Array(const Array &other):_arr(NULL),_length(other._length){
+
+			if(_length > 0){
+				_arr = new T[_length]();
+			for(size_t i = 0; i < _length ; ++i)
+				_arr[i]=other._arr[i];
+			}
+		
+		}
+		Array &operator=(const Array &other){
+			
+			if(this == &other)
+				return *this;
+			delete[] _arr;
+			this->_length = other._length;
+			this->_arr = NULL;
+			
+			if(_length > 0){
+				this->_arr = new T[_length]();
+				for(size_t i = 0; i < _length ; ++i)
+					_arr[i]=other._arr[i];
+			}
+			return *this;
+		}
+		
+		~Array(){
+			delete[] _arr;
+		}
+		
+		T &operator[](unsigned int index){
+			if(index >=_length)throw std::exception();
+			return _arr[index];
+		}
+		const T &operator[](unsigned int index)const{
+			if(index >=_length)throw std::exception();
+			return _arr[index];
+		}
+		size_t size()const{
+			return _length;
+		}
+	private:
+		T* _arr;
+		size_t _length;
+};
 #endif
--- rank5/CPP07/ex02/main.cpp
+++ rank5/CPP07/ex02/main.cpp
@@ -1,19 +1,88 @@
+#include <iostream>
 #include "Array.hpp"
 
-template <class A> class abc{
+int main() {
 
-	public:
-		A num1;
-		A num2;
-		abc(A n1,A n2):num1(n1),num2(n2){}	
-		void getValues(){std::cout<<"num1: "<<num1<<" num2: "<<num2;}	
-	private:
+    std::cout << "\033[95m================= DEFAULT CONSTRUCTOR =================\033[0m\n";
+    {
+        Array<int> a;
+        std::cout << "Size: " << a.size() << std::endl;
+    }
 
-};
+    std::cout << "\033[96m================ PARAMETERIZED CONSTRUCTOR =============\033[0m\n";
+    {
+        Array<int> a(5);
+        for (unsigned int i = 0; i < a.size(); ++i)
+            a[i] = i * 10;
+        for (unsigned int i = 0; i < a.size(); ++i)
+            std::cout << a[i] << " ";
+        std::cout << std::endl;
+    }
 
-int main(){
-	abc<int> a(1,2);
-	a.getValues();
-	abc<int> b(1.1,1);
-	b.getValues();
+    std::cout << "\033[92m==================== COPY CONSTRUCTOR ==================\033[0m\n";
+    {
+        Array<int> a(3);
+        for (unsigned int i = 0; i < a.size(); ++i)
+            a[i] = i + 1;
+
+        Array<int> b(a);
+        b[0] = 42;
+
+        std::cout << "Original: ";
+        for (unsigned int i = 0; i < a.size(); ++i)
+            std::cout << a[i] << " ";
+        std::cout << "\nCopy:     ";
+        for (unsigned int i = 0; i < b.size(); ++i)
+            std::cout << b[i] << " ";
+        std::cout << std::endl;
+    }
+
+    std::cout << "\033[93m================= ASSIGNMENT OPERATOR ==================\033[0m\n";
+    {
+        Array<int> a(4);
+        for (unsigned int i = 0; i < a.size(); ++i)
+            a[i] = i;
+
+        Array<int> b;
+        b = a;
+        b[1] = 99;
+
+        std::cout << "Original: ";
+        for (unsigned int i = 0; i < a.size(); ++i)
+            std::cout << a[i] << " ";
+        std::cout << "\nAssigned: ";
+        for (unsigned int i = 0; i < b.size(); ++i)
+            std::cout << b[i] << " ";
+        std::cout << std::endl;
+    }
+
+    std::cout << "\033[94m=================== CONST ACCESS TEST ==================\033[0m\n";
+    {
+        Array<int> a(2);
+        a[0] = 7;
+        a[1] = 14;
+        const Array<int> b(a);
+
+        std::cout << b[0] << " " << b[1] << std::endl;
+    }
+
+    std::cout << "\033[91m================= OUT OF BOUNDS TEST ===================\033[0m\n";
+    {
+        Array<int> a(1);
+        try {
+            std::cout << a[5] << std::endl;
+        } catch (const std::exception&) {
+            std::cout << "Exception caught" << std::endl;
+        }
+    }
+
+    std::cout << "\033[90m==================== EMPTY ASSIGNMENT ==================\033[0m\n";
+    {
+        Array<int> a(3);
+        Array<int> b;
+        b = a;
+        std::cout << "Size after assignment: " << b.size() << std::endl;
+    }
+
+    return 0;
 }
